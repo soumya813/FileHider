38b3c297d42440c99dac66ec9aca231a23a7199d
Soumya Srivastav <soumyasrivastav53@gmail.com>
2025-03-28 12:33:55 +0530

    jdbc connection

--- src/main/java/db/MyConnection.java
+++ src/main/java/db/MyConnection.java
@@ -1,4 +1,27 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MyConnection {
+    public static Connection connection = null;
+    public static Connection getConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/filehider?useSSL=false","root","Tashu@1523915");
+        } catch (ClassNotFoundException | SQLException e ) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+    public static void closeConnection(){
+        if(connection != null){
+            try{
+                connection.close();
+            }catch(SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
 }
